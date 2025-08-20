c1b8e856597a3fde9145f8a7044618cd6fd7a561
Ivan-D4 <ivannicolasdeleon@hotmail.com>
2025-08-20 12:25:30 -0300

    avance clase 1

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,66 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+class nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior = None
+
+class listaDobleEnlazada:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamanio = 0
+
+def agregar_al_inicio(self,dato):
+    nuevo_nodo = nodo(dato)
+    if self.cabeza is None:
+       self.cabeza = nuevo_nodo
+       self.cola = nuevo_nodo
+    else:
+        nuevo_nodo.siguiente = self.cabeza
+        self.cabeza.anterior = nuevo_nodo
+        self.cabeza = nuevo_nodo
+    self.tamanio += 1
+
+    def agregar_al_final(self,dato):
+     nuevo_nodo = nodo(dato)
+     if self.cabeza is None:
+       self.cabeza = nuevo_nodo
+       self.cola = nuevo_nodo
+     else:
+        nuevo_nodo.siguiente = self.cola
+        self.cola.anterior = nuevo_nodo
+        self.cola = nuevo_nodo
+    self.tamanio += 1
+
+
+    def insertar(self,dato,posicion):
+        if posicion < 0 or posicion > self.tamanio:
+            raise Exception("Posición inválida")
+        if posicion == 0:
+            self.agregar_al_inicio(dato)
+        elif posicion == self.tamanio:
+            self.agregar_al_final(dato)
+        else:
+            nuevo_nodo = nodo(dato)
+            actual = self.cabeza
+            for _ in range(posicion):
+                actual = actual.siguiente
+
+            nuevo_nodo.anterior = actual.anterior
+            nuevo_nodo.siguiente = actual
+            actual.anterior.siguiente = nuevo_nodo
+            actual.anterior = nuevo_nodo
+            self.tamanio += 1
+
+
+
+
+
+
+
+
+
+
+
